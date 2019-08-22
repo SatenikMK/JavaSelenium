796aa43bf7f4fea49a9a4bae13f2a52e6ca0aacc
Satenik Mkrtchyan <satenik.mkrtchyan@monitis.com>
2019-08-22 17:36:14 +0400

    first commit

--- CelsiusToFahreinheit.java
+++ CelsiusToFahreinheit.java
@@ -1,6 +1,6 @@
 public class CelsiusToFahreinheit {
     public static double convertToFahrenheit(double temp){
-        return temp*5/9 + 32;
+        return (temp*5/9) + 32;
     }
     public static void main(String args []){
        double temp = 102;
